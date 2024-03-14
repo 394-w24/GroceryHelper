1991245e9ab59f6f88d2d07f992464d7ab67ecb8
DooHwanKim0419 <dkimh0419@gmail.com>
2024-03-13 19:37:44 -0500

    removed firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import {
   getFirestore,
@@ -27,18 +26,17 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDgvfRM8_g9dPdMiEooaU-q4lBUDHqz3TA",
-  authDomain: "groceryhelper-fdb2d.firebaseapp.com",
-  projectId: "groceryhelper-fdb2d",
-  storageBucket: "groceryhelper-fdb2d.appspot.com",
-  messagingSenderId: "4854415838",
-  appId: "1:4854415838:web:6295f2bab2abdb0c78dd4f",
-  measurementId: "G-TXR6SHT9B4",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_API_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const storage = getStorage(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
